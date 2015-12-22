c91539309644772703dbddce82c1c076cd058a78
Sachin Dhingra <sachin.dhingra30@gmail.com>
2015-12-22 15:02:12 +0530

    Create SkiingProblem.java
    Ram likes skiing a lot. That's not very surprising, since skiing is really great. The problem with skiing is one have to slide downwards to gain speed. Also when reached the bottom most point  one has to wait for ski-lift to get to higher altitude.
    
    Ram seeks your help to know the longest run possible with the given peaks. That altitude of different peaks is given by a grid of numbers. Look at this example:
    
    7  2  3  4  5 
    36 37 38 34 6 
    33 44 46 40 7 
    24 43 42 41 8 
    35 32 47 30 9
    
    One can ski down from one peak to a connected peak if and only if the height decreases. One peak is connected to another if it's at left, at right, above or below it. In the sample map, a possible ski path would be 36-33-24(start at 36, end at 24). Of course if one would ski 46-44-43-42-41-30-9....3-2, it would be a much longer run. In fact, it's the longest possible. There could be more than one longest ski path, but all Ram needs from you is to tell maximum number of peaks he could cover for a given map, in this case it is 14.
    
    Input
    All input comes from input.txt file. The first line contains the number of test cases N. Each test case starts with a line containing the name (it's a single string), the number of rows R and the number of columns C. After that follow R lines with C numbers each, defining the heights. R and C won't be bigger than 100.
    
    Output 
    For each test case, print a line to output.txt containing the name of the area, a colon, a space and the length of the longest run (maximum points covered) one can slide down in that area. 
    
    Sample Input
    2
    Manali 10 5
    56 14 51 58 88
    26 94 24 39 41
    24 16 8 51 51
    76 72 77 43 10
    38 50 59 84 81
    5 23 37 71 77
    96 10 93 53 82
    94 15 96 69 9
    74 0 62 38 96
    37 54 55 82 38
    Narkanda 5 5	
    1 2 3 4 5
    16 17 18 19 6
    15 24 25 20 7
    14 23 22 21 8
    13 12 11 10 9
    
    Sample Output
    Manali: 7
    Narkanda: 25

---
+++ com/sachin/puzzles/corejava/SkiingProblem.java
@@ -0,0 +1,199 @@
+/**
+ * 
+ */
+package com.sachin.puzzles.corejava;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * @author Sachin Dhingra
+ *
+ */
+public class SkiingProblem {
+
+	// Check if the new problem started.
+	private boolean isTokenStartWithString(String[] tokens) {
+		try{
+			Integer.valueOf(tokens[0]);
+			return false;
+		}catch(NumberFormatException e) {
+			return true;
+		}
+	}
+
+	// Traverse all nodes in matrix and find the longest ski path
+	private int findLongestRoute(Integer[][] matrix)
+	{
+		String longestSkiPath = "";
+
+		//rows
+		for(int i = 0; i<matrix.length; i++)
+		{   //cols
+			for(int j = 0; j<matrix[i].length; j++)
+			{
+				String returnSkiPath = findLongestRouteOfNode(matrix, i, j);
+
+				if(returnSkiPath.length() > longestSkiPath.length()) {
+					longestSkiPath = returnSkiPath;
+				}
+			}
+		}
+
+		//	        System.out.println(longestSkiPath.toString());
+		return longestSkiPath.split(" ").length;	        
+
+	}
+
+	// Check the left, right, upper and down longest ski path and return the max of them.
+	private String findLongestRouteOfNode(Integer[][] matrix, int row, int col)
+	{
+		String maxPath = getLeftPath(matrix, row, col);
+
+		String rightRun = getRightPath(matrix, row, col);
+
+		if(rightRun.length() > maxPath.length())
+			maxPath = rightRun;
+
+		String upperRun = getUpperPath(matrix, row, col);
+
+		if(upperRun.length() > maxPath.length())
+			maxPath = upperRun;
+
+		String downRun = getDownPath(matrix, row, col);
+
+		if(downRun.length() > maxPath.length())
+			maxPath = downRun;
+
+
+		if (!maxPath.isEmpty())
+			maxPath += " " + matrix[row][col];
+		else
+			maxPath += matrix[row][col];
+		return maxPath;
+	}
+
+	// Get longest ski path for left index.
+	private String getLeftPath(Integer[][] matrix, int row, int col)
+	{
+		String leftRun = "";
+		int left = col-1;
+
+		// check if left exist and its value is less than current index.
+		if(left >= 0 && matrix[row][left] < matrix[row][col]) {	            
+			leftRun = findLongestRouteOfNode(matrix, row, left);
+		}
+		return leftRun;
+	}
+
+	// Get longest run for right index.
+	private String getRightPath(Integer[][] matrix, int row, int col)
+	{
+		String rightRun = "";
+		int right = col+1;
+
+		// check if right exist and its value is less than current index.
+		if(right < matrix[0].length && matrix[row][right] < matrix[row][col]) {
+			rightRun = findLongestRouteOfNode(matrix, row, right);
+		}
+
+		return rightRun;
+	}
+
+	// Get longest run for upper index.
+	private String getUpperPath(Integer[][] matrix, int row, int col)
+	{
+		String upperRun = "";
+		int up = row-1;
+
+		// check if upper index exist and its value is less than current index.
+		if(up >= 0 && matrix[up][col] < matrix[row][col]) {
+			upperRun =  findLongestRouteOfNode(matrix, up, col);
+		}
+
+		return upperRun;
+	}
+
+	// Get longest run for down index.
+	private String getDownPath(Integer[][] matrix, int row, int col)
+	{
+		String downRun = "";
+		int down = row+1;
+
+		// check if down index exist and its value is less than current index.
+		if(down < matrix.length && matrix[down][col] < matrix[row][col]) {
+			downRun = findLongestRouteOfNode(matrix, down, col);
+		}
+
+		return downRun;
+	}	    
+
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Output:");
+		BufferedReader br = null;
+		SkiingProblem skiingObj = new SkiingProblem();
+		Integer[][] matrix = null;
+		String currentLine = null;
+		int rowCount = 0;
+		String name = null;
+		int numOfProbs;
+
+		// Read file and create 2D array
+		try {
+
+			br = new BufferedReader(new FileReader("src/test/resources/input.txt"));
+			currentLine = br.readLine();
+			numOfProbs = Integer.parseInt(currentLine);
+			currentLine = br.readLine();
+
+			while (currentLine  != null && numOfProbs>=0) {
+				String[] tokens =  currentLine.split(" ");
+
+				if(skiingObj.isTokenStartWithString(tokens))
+				{
+					if(matrix != null)
+					{
+						System.out.println(name + ": "+ skiingObj.findLongestRoute(matrix));
+						matrix = null;
+					}
+					name = tokens[0];
+					rowCount = 0;
+					Integer row =  Integer.valueOf(tokens[1]);
+					Integer col =  Integer.valueOf(tokens[2]);
+					matrix = new Integer[row][col];
+					numOfProbs--;
+				} else
+				{
+					for(int i = 0; i < tokens.length; i++)
+					{
+						matrix[rowCount][i] = Integer.valueOf(tokens[i]);
+					}
+					rowCount++;
+				}
+				currentLine = br.readLine();
+			}
+
+			//For the last problem
+			if(matrix != null && numOfProbs>=0)
+			{
+				System.out.println(name + ": "+ skiingObj.findLongestRoute(matrix));
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (br != null)
+					br.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+}
